ce4a7cc48d9b7a176e70b3db15de3735b93db4b0
suzy.uemura@gft.com <siua@gft.com>
2021-10-22 10:06:43 -0300

    create client

--- api/controllers/ClientsController.js
+++ api/controllers/ClientsController.js
@@ -5,6 +5,7 @@ class ClientsController {
      * pega todos os clientes pelo ClientsService
      * @param {*} req 
      * @param {*} res 
+     * @return lista de objetos 
      */
     static async getAllclients(req, res) {
         try {
@@ -12,7 +13,7 @@ class ClientsController {
             console.log('teste controller' + clients)
             res.status(200).json(clients)
         } catch (error) {
-            res.status(404).json({menssage: error.toString()})
+            res.status(404).json({message: error.toString()})
         }
     }
 
@@ -20,14 +21,30 @@ class ClientsController {
      * Pega um cliente pela service
      * @param {*} req.params.id
      * @param {*} res 
-     */
+     * @return lista de objetos
+     */ 
     static async getOneClient(req, res) {
         try {
             const clientId = req.params.id
             const client = await ClientsService.findOneClient(clientId)
             res.status(200).json(client)
         } catch (error) {
-            res.status(404).json({menssage: error.toString()})
+            res.status(404).json({message: error.toString()})
+        }
+    }
+
+    /**
+     * criar um novo cliente pelo service pegando do corpo da requisição
+     * @param {*} req.boy
+     * @param {*} res 
+     */
+    static async postClient(req, res) {
+        try {
+            const dadosNovoCliente = req.body
+            const novoCliente= await ClientsService.criarCliente(dadosNovoCliente)
+            res.status(201).json(novoCliente)
+        } catch (error) {
+            res.status(404).json({message: error.toString()})
         }
     }
 }
